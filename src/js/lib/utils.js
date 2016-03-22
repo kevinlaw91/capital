29cca22902fc7fe5ded3ef9ad455ea4528555aa2
Kevin Law <hao_21291@yahoo.com>
2016-03-23 00:42:06 +0800

    Added clamp() method to utils module

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -1,4 +1,21 @@
 define(function(){
+	/** @module lib/utils */
+	var Utils = {
+		clamp: function(value, min, max) {
+			/**
+			 * Returns a number whose value is limited to the given range.
+			 *
+			 * Example: limit the output of this computation to between 0 and 255
+			 * (x * 255).clamp(0, 255)
+			 *
+			 * @param {number} min The lower boundary of the output range
+			 * @param {number} max The upper boundary of the output range
+			 * @returns {number} A number in the range [min, max]
+			 */
+			return Math.min(Math.max(value, min), max);
+		}
+	};
+
 	(function(global) {
 		/** JSON.minify()
 		 *  v0.1 (c) Kyle Simpson
@@ -69,4 +86,6 @@ define(function(){
 			return new_str.join("");
 		};
 	})(window); //export to window object
+
+	return Utils;
 });
