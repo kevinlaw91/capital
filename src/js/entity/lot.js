0339c0a90e0b48cf97a1e69bcc933348a8aff527
Kevin Law <hao_21291@yahoo.com>
2016-03-23 17:56:29 +0800

    Added cost randomizer

--- src/js/entity/lot.js
+++ src/js/entity/lot.js
@@ -99,14 +99,13 @@ define([
 		this.buildingX = props.b.x;
 		this.buildingY = props.b.y;
 
-		/** Cost */
-		this.cost = {
-			land: 0,
-			upgrade1: 0,
-			upgrade2: 0,
-			upgrade3: 0,
-			upgrade4: 0
-		};
+		/**
+		 * Cost
+		 * @type {object}
+		 */
+		if(props.cost !== "undefined"){
+			this.cost = props.cost;
+		}
 
 		/**
 		 * Color marker that shows the owner
--- src/js/entity/map.js
+++ src/js/entity/map.js
@@ -1,4 +1,8 @@
-define(["entity/lot"], function(Lot) {
+define([
+	"jquery",
+	"entity/lot",
+    "utils"
+], function($, Lot) {
 	'use strict';
 	return {
 		/**
@@ -6,16 +10,80 @@ define(["entity/lot"], function(Lot) {
 		 * @returns {Lot[]}
 		 */
 		generate: function(){
+			// Import Utils
+			var Utils = require("utils");
+
+			/**
+			 * Returns a standard normal (gaussian) random number generator function
+			 * Generator was implemented using Marsaglia polar method
+			 */
+			function gaussian( mean, stDev ) {
+				return function() {
+					var y, u, v, s;
+					do {
+						u = 2.0 * Math.random() - 1.0;
+						v = 2.0 * Math.random() - 1.0;
+						s = u * u + v * v;
+					}
+					while(s>=1.0 || s===0);
+					s = Math.sqrt((-2.0 * Math.log(s)) / s);
+					y = u * s; // Interchangeable with v * s
+					return mean + stDev * y;
+				};
+			}
+
+			function randomAmount(dist, min, max){
+				// Increment of 100
+				var unit = 100;
+				// ~~ was used to strip decimals
+				return Utils.clamp(~~(dist()/unit)*unit,min,max);
+			}
+
+			/**
+			 * Map Generator
+			 */
 			time("Generate Map For New Game");
+
 			/**
 			 * Map definition
 			 * @type {Lot[]}
 			 */
 			var map = [];
 
-			/**
-			 * Begin Generator Script
-			 */
+			// Constants
+			var tradable = { tradable: true },
+			    nd = {
+				    /**
+				     * Normal distributions
+				     *
+				     * arg1 = mean, expected average of output
+				     * arg2 = stDev, range of output +/- from mean
+				     */
+					land: gaussian(7000,3000),
+					upg1: gaussian(2500,1500),
+					upg2: gaussian(1500,1000),
+					upg3: gaussian(2800,2000),
+					upg4: gaussian(3000,1500)
+			    };
+
+			function generateCost(){
+				var cost_land = randomAmount(nd.land,1000,13000),
+				cost_upgrade1 = randomAmount(nd.upg1,500,4500),
+				cost_upgrade2 = cost_upgrade1 + randomAmount(nd.upg2,500,2500),
+				cost_upgrade3 = cost_upgrade2 + randomAmount(nd.upg3,1000,3000),
+				cost_upgrade4 = cost_upgrade3 + randomAmount(nd.upg4,1500,4000);
+
+				return {
+					cost: {
+						land: cost_land,
+						upgrade1: cost_upgrade1,
+						upgrade2: cost_upgrade2,
+						upgrade3: cost_upgrade3,
+						upgrade4: cost_upgrade4
+					}
+				};
+			}
+
 			var i;
 
 			// South corner
@@ -31,12 +99,13 @@ define(["entity/lot"], function(Lot) {
 			// Lot in south west
 			for(i=12; i>=4; i--){
 				map.push(
-					new Lot({
-						pos: { x: i, y: 13},
-						b:   { x: i, y: 15},
-						direction: Lot.FACING_SOUTH,
-						tradable: true
-					})
+					new Lot(
+						$.extend(tradable, generateCost(), {
+							pos: { x: i, y: 13},
+							b:   { x: i, y: 15},
+							direction: Lot.FACING_SOUTH
+						})
+					)
 				);
 			}
 
@@ -53,12 +122,13 @@ define(["entity/lot"], function(Lot) {
 			// Lot in north west
 			for(i=12; i>=4; i--){
 				map.push(
-					new Lot({
-						pos: { x: 3, y: i},
-						b:   { x: 1, y: i},
-						direction: Lot.FACING_EAST,
-						tradable: true
-					})
+					new Lot(
+						$.extend(tradable, generateCost(), {
+							pos: { x: 3, y: i},
+							b:   { x: 1, y: i},
+							direction: Lot.FACING_EAST
+						})
+					)
 				);
 			}
 
@@ -75,12 +145,13 @@ define(["entity/lot"], function(Lot) {
 			// Lot in north east
 			for(i=4; i<=12; i++){
 				map.push(
-					new Lot({
-						pos: { x: i, y: 3},
-						b:   { x: i, y: 1},
-						direction: Lot.FACING_SOUTH,
-						tradable: true
-					})
+					new Lot(
+						$.extend(tradable, generateCost(), {
+							pos: { x: i, y: 3},
+							b:   { x: i, y: 1},
+							direction: Lot.FACING_SOUTH
+						})
+					)
 				);
 			}
 
@@ -97,12 +168,13 @@ define(["entity/lot"], function(Lot) {
 			// Lot in south east
 			for(i=4; i<=12; i++){
 				map.push(
-					new Lot({
-						pos: { x: 13, y: i},
-						b:   { x: 15, y: i},
-						direction: Lot.FACING_EAST,
-						tradable: true
-					})
+					new Lot(
+						$.extend(tradable, generateCost(), {
+							pos: { x: 13, y: i},
+							b:   { x: 15, y: i},
+							direction: Lot.FACING_EAST
+						})
+					)
 				);
 			}
 
