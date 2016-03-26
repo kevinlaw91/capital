4aa2fb3dedb1f0002fc02fe0623b974320e3157e
Kevin Law <hao_21291@yahoo.com>
2016-03-26 18:48:36 +0800

    Reworked popup text animations

--- src/js/render/script/popup.js
+++ src/js/render/script/popup.js
@@ -4,6 +4,10 @@ define([
 	"engine/renderer"
 ], function(Snap, AssetManager) {
 	'use strict';
+
+	var animation_path_str = "M{start.x},{start.y} C{start.cX},{start.cY} {end.cX},{end.cY}  {end.x},{end.y}",
+		customEaseIn = function(n) { return Math.pow(n,0.68); };
+
 	/**
 	 * @function
 	 * @param {string} msg - Message to be rendered
@@ -17,22 +21,70 @@ define([
 	 * @param {string} [options.prefixColor=white] - Color for prefix text
 	 */
 	return function(msg, options) {
-		var posX = (options && options.x) || 0;
-		var posY = (options && options.y) || 0;
+		var posX = (options && options.x) || 0,
+		    posY = (options && options.y) || 0;
 
-		var newGroup = require("engine/renderer").layers.popups.paper.g().attr({ opacity: 0});
+		var newGroup = require("engine/renderer").layers.popups.paper.g();
 
 		//
-		// Text
+		// Create Animation Path
 		//
+		var rise = 25 + Math.random() * 15,
+		    horizontal_range = 20, // Move left or right (max to this amount)
+		    vertical_range = 45, // Move to bottom (max to this amount)
+		    end_dX = Math.floor(Math.random() * (horizontal_range * 2 + 1)) - (horizontal_range),
+		    end_dY = 15 + Math.floor(Math.random() * (vertical_range + 1)),
+		    path = newGroup.path(
+			    Snap.format(animation_path_str, {
+						start: {
+							// Starting Point
+							x: posX,
+							y: posY,
+							// Control Point
+							cX: posX,
+							cY: posY - rise
+						},
+						end: {
+							// Ending Point
+							x: posX + end_dX,
+							y: posY + end_dY,
+							// Control Point
+							cX: posX + end_dX,
+							cY: posY - rise
+						}
+				})
+		    ).attr({
+				stroke: "none",
+				fill: "none"
+			});
 
-		//Draw message
-		var text = newGroup.text(posX, posY, msg).attr({
+		//
+		// Contents
+		//
+		var scaleGroup = newGroup.g(),
+		    animateGroup = scaleGroup.g().attr({ opacity: 0});
+
+		// Draw message
+		// Shadow
+		var textShadow = animateGroup.text(posX, posY, msg).attr({
 			//Styles
-			fontFamily: 'Advent Pro',
-			fontWeight: 800,
+			fontFamily: 'Roboto Condensed',
+			fontSize: "12px",
+			fontWeight: 900,
+			letterSpacing: "-0.25px",
+			fill: "rgba(0, 0, 0, 0.33)"
+		});
+
+		// Text
+		var text = animateGroup.text(posX, posY, msg).attr({
+			//Styles
+			fontFamily: 'Roboto Condensed',
+			fontSize: "12px",
+			fontWeight: 900,
+			letterSpacing: "-0.25px",
 			fill: "white"
 		});
+
 
 		//Change color
 		if(options && options.color) {
@@ -42,9 +94,15 @@ define([
 		}
 
 		//Align center
-		text.attr({
+		var center = {
 			x : posX - (text.getBBox().width/2),
 			y : posY + (text.getBBox().height/2) - /* Extra bottom padding */ 15
+		};
+
+		text.attr(center);
+		textShadow.attr(center);
+		textShadow.attr({
+			y: textShadow.asPX("y") + 0.5
 		});
 
 		//Message position finalized.
@@ -54,11 +112,11 @@ define([
 		//Draw prefix
 		if(options && typeof options.prefix != "undefined"){
 			//Draw at baseline
-			var prefix_text = newGroup.text(posX, posY, options.prefix).attr({
+			var prefix_text = animateGroup.text(posX, posY, options.prefix).attr({
 				//Styles
-				fontFamily: 'Advent Pro',
-				fontSize: "24px",
-				fontWeight: 800,
+				fontFamily: 'Roboto',
+				fontSize: "14px",
+				fontWeight: 900,
 				fill: "white"
 			});
 
@@ -75,7 +133,7 @@ define([
 
 			//Move to left and vertically center with message
 			prefix_text.attr({
-				x : text.node.x.baseVal[0].value - prefix_text.getBBox().width - /* Extra right padding */ 5,
+				x : text.node.x.baseVal[0].value - prefix_text.getBBox().width - /* Extra right padding */ 2,
 				y : prefix_text.node.y.baseVal[0].value - diff
 		    });
 		}
@@ -94,7 +152,7 @@ define([
 		//Draw icon
 		if(options && options.icon){
 			if(AssetManager.hasSymbol(options.icon)) {
-				var icon = newGroup.use(options.icon);
+				var icon = animateGroup.use(options.icon);
 				var iconStyles = {
 					x: iconPosition.x,
 					y: iconPosition.y,
@@ -117,20 +175,37 @@ define([
 		// Animations
 		//
 
-		//Fade in for 300ms
-		newGroup.animate({
-			opacity: 1,
-			transform: "translate(0,-10)"
-		}, 300, mina.easein);
+		// Random life
+		var life = Math.random() * 1500;
 
-		//Fade out after 2300ms
+		// Fade in for 1000ms
+		animateGroup.animate({
+			opacity: 1
+		}, 1000, mina.easeout);
+
+		Snap.animate(0, 1, function(val){
+			// Move along path
+			var length = path.getTotalLength(),
+			    point = path.getPointAtLength(length * val),
+			    matrix = new Snap.matrix();
+			matrix.translate(point.x - posX, point.y - posY);
+			animateGroup.transform(matrix);
+
+			// Scale
+			var bb = scaleGroup.getBBox(),
+				scale = 1.5-Math.abs(val - 0.25);
+			matrix = new Snap.matrix();
+			matrix.scale(scale, scale, bb.cx, bb.cy);
+			scaleGroup.transform(matrix);
+		}, 4500 + life, customEaseIn);
+
+		// Fade out
 		window.setTimeout(function(){
-			newGroup.animate({
-				opacity: 0,
-				transform: "translate(0,-20)"
-			}, 300, mina.easeout, function(){
+			animateGroup.animate({
+				opacity: 0
+			}, 1000, mina.easeout, function(){
 				Snap(newGroup).remove();
 			});
-		}, 2300);
+		}, 4500 + life - 1000);
 	};
 });
