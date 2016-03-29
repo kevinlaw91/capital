6e9ecc1e67dc36f942f208cf8fbae01d42a4488c
Kevin Law <hao_21291@yahoo.com>
2016-03-29 19:50:00 +0800

    [FIXED] Performance drop when popup text is visible

--- src/js/render/script/popup.js
+++ src/js/render/script/popup.js
@@ -5,8 +5,7 @@ define([
 ], function(Snap, AssetManager) {
 	'use strict';
 
-	var animation_path_str = "M{start.x},{start.y} C{start.cX},{start.cY} {end.cX},{end.cY}  {end.x},{end.y}",
-		customEaseIn = function(n) { return Math.pow(n,0.68); };
+	var customEaseIn = function(n) { return Math.pow(n,0.68); };
 
 	/**
 	 * @function
@@ -29,31 +28,17 @@ define([
 		//
 		// Create Animation Path
 		//
-		var rise = 25 + Math.random() * 15,
+		var r1 = Math.random(), r2 = Math.random(), r3 = Math.random(),
+			rise =  25 /* minimum */ + r1 * 15,
 		    horizontal_range = 20, // Move left or right (max to this amount)
 		    vertical_range = 45, // Move to bottom (max to this amount)
-		    end_dX = Math.floor(Math.random() * (horizontal_range * 2 + 1)) - (horizontal_range),
-		    end_dY = 15 + Math.floor(Math.random() * (vertical_range + 1)),
-		    path = newGroup.path(
-			    Snap.format(animation_path_str, {
-						start: {
-							// Starting Point
-							x: posX,
-							y: posY,
-							// Control Point
-							cX: posX,
-							cY: posY - rise
-						},
-						end: {
-							// Ending Point
-							x: posX + end_dX,
-							y: posY + end_dY,
-							// Control Point
-							cX: posX + end_dX,
-							cY: posY - rise
-						}
-				})
-		    ).attr({
+		    end_dX = Math.floor(r2 * (horizontal_range * 2 + 1)) - (horizontal_range),
+		    end_dY = 15 + Math.floor(r3 * (vertical_range + 1)),
+		    path_str = "M" + posX + "," + posY +
+		               " C" + posX + "," + (posY - rise) + " " +
+		               (posX + end_dX) + "," + (posY - rise) + " " +
+		               (posX + end_dX) + "," + (posY + end_dY),
+			path = newGroup.path(path_str).attr({
 				stroke: "none",
 				fill: "none"
 			});
@@ -61,13 +46,15 @@ define([
 		//
 		// Contents
 		//
-		var scaleGroup = newGroup.g(),
-		    animateGroup = scaleGroup.g().attr({ opacity: 0});
+		var animateGroup = newGroup.g().attr({ opacity: 0});
 
 		// Draw message
-		// Shadow
-		var textShadow = animateGroup.text(posX, posY, msg).attr({
+
+		// Shadow (behind) will be drawn first
+		var textShadow = animateGroup.text(0, 0, msg).attr({
 			//Styles
+			dominantBaseline: "central",
+			textAnchor: "middle",
 			fontFamily: 'Roboto Condensed',
 			fontSize: "12px",
 			fontWeight: 900,
@@ -75,9 +62,11 @@ define([
 			fill: "rgba(0, 0, 0, 0.33)"
 		});
 
-		// Text
-		var text = animateGroup.text(posX, posY, msg).attr({
+		// Text (foreground) will be drawn later
+		var text = animateGroup.text(0, 0, msg).attr({
 			//Styles
+			dominantBaseline: "central",
+			textAnchor: "middle",
 			fontFamily: 'Roboto Condensed',
 			fontSize: "12px",
 			fontWeight: 900,
@@ -86,38 +75,32 @@ define([
 		});
 
 
-		//Change color
+		// Change color
 		if(options && options.color) {
 			text.attr({
 				fill: options.color
 			});
 		}
 
-		//Align center
-		var center = {
-			x : posX - (text.getBBox().width/2),
-			y : posY + (text.getBBox().height/2) - /* Extra bottom padding */ 15
-		};
+		// Shadow offset
+		textShadow.transform("translate(0, 1)");
 
-		text.attr(center);
-		textShadow.attr(center);
-		textShadow.attr({
-			y: textShadow.asPX("y") + 0.5
-		});
-
-		//Message position finalized.
-		//Use text bounding box as anchor point for other elements
+		// Use text bounding box as anchor point for other elements
 		var anchor = text.getBBox();
 
-		//Draw prefix
+		// Draw prefix
 		if(options && typeof options.prefix != "undefined"){
 			//Draw at baseline
-			var prefix_text = animateGroup.text(posX, posY, options.prefix).attr({
+			var prefix_text = animateGroup.text(0, 0, options.prefix).attr({
 				//Styles
+				dominantBaseline: "central",
+				textAnchor: "end",
 				fontFamily: 'Roboto',
-				fontSize: "14px",
+				fontSize: "18px",
 				fontWeight: 900,
-				fill: "white"
+				fill: "white",
+				x: anchor.x - /* Extra right padding */ 2,
+				y: 0
 			});
 
 			//Change color
@@ -126,31 +109,21 @@ define([
 					fill: options.prefixColor
 				});
 			}
-
-			//Calculate the diff between message & prefix vertical center point
-			var centerY_prefix = prefix_text.getBBox().y + (prefix_text.getBBox().height / 2),
-				diff = centerY_prefix - (anchor.y + (anchor.height / 2)); //center prefix - center message
-
-			//Move to left and vertically center with message
-			prefix_text.attr({
-				x : text.node.x.baseVal[0].value - prefix_text.getBBox().width - /* Extra right padding */ 2,
-				y : prefix_text.node.y.baseVal[0].value - diff
-		    });
 		}
 
 		//
 		// Icon
 		//
 
-		//Centered
-		var iconSize = 24;
-		var iconPosition = {
-			x: posX - (iconSize / 2),
-			y: anchor.y - iconSize
-		};
-
 		//Draw icon
 		if(options && options.icon){
+			//Centered
+			var iconSize = 24,
+			    iconPosition = {
+					x: -(iconSize / 2),
+					y: anchor.y - iconSize
+				};
+
 			if(AssetManager.hasSymbol(options.icon)) {
 				var icon = animateGroup.use(options.icon);
 				var iconStyles = {
@@ -175,37 +148,44 @@ define([
 		// Animations
 		//
 
-		// Random life
-		var life = Math.random() * 1500;
+		// Randomize life span
+		var life = r1 * 1500;
 
 		// Fade in for 1000ms
-		animateGroup.animate({
-			opacity: 1
-		}, 1000, mina.easeout);
+		animateGroup.animate(
+			{ opacity: 1 },
+			1000,
+			mina.easeout
+		);
 
-		Snap.animate(0, 1, function(val){
-			// Move along path
-			var length = path.getTotalLength(),
-			    point = path.getPointAtLength(length * val),
-			    matrix = new Snap.matrix();
-			matrix.translate(point.x - posX, point.y - posY);
-			animateGroup.transform(matrix);
+		// Path animation function
+		var pathAnimation = (function(p){
+			var l = p.node.getTotalLength();
 
-			// Scale
-			var bb = scaleGroup.getBBox(),
-				scale = 1.5-Math.abs(val - 0.25);
-			matrix = new Snap.matrix();
-			matrix.scale(scale, scale, bb.cx, bb.cy);
-			scaleGroup.transform(matrix);
-		}, 4500 + life, customEaseIn);
+			return function(val){
+				var scale = 1.5-Math.abs(val - 0.25),
+				    length = l,
+				    point = path.getPointAtLength(length * val),
+
+				    // Optimized transform matrix, included operations:
+				    // matrix.scale(scale, scale, point.x, point.y);
+				    // matrix.translate(point.x, point.y);
+				    matrix = new Snap.matrix(scale,0,0,scale,point.x,point.y);
+
+				animateGroup.transform(matrix);
+			};
+		})(path);
+		// Start path animation
+		Snap.animate(0, 1, pathAnimation, 4500 + life, customEaseIn);
 
 		// Fade out
 		window.setTimeout(function(){
-			animateGroup.animate({
-				opacity: 0
-			}, 1000, mina.easeout, function(){
-				Snap(newGroup).remove();
-			});
+			animateGroup.animate(
+				{ opacity: 0 },
+				1000,
+				mina.easeout,
+				function(){ Snap(newGroup).remove(); }
+			);
 		}, 4500 + life - 1000);
 	};
 });
