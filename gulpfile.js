df395b3dc36f425a8c999d67ec09592b57b67274
Kevin Law <hao_21291@yahoo.com>
2016-06-25 04:02:19 +0800

    Switched to SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-var gulp = require("gulp");
-var svgSymbols = require('gulp-svg-symbols');
-var svgmin = require("gulp-svgmin");
-var rename = require("gulp-rename");
+// Imports
+var gulp = require("gulp"),
+	svgSymbols = require('gulp-svg-symbols'),
+	svgmin = require("gulp-svgmin"),
+	rename = require("gulp-rename"),
+	sass = require('gulp-sass');
 
-// root directory for resources that need to be packed
-var pack_root = "dev/packing/";
-var pack_templates_dir = pack_root + "templates/";
-var pack_output_dir = pack_root + "output/";
+/*
+ * SVG Symbols Packing
+ */
+
+// Root directory for resources that need to be packed
+var pack_root = "dev/packing/",
+	pack_templates_dir = pack_root + "templates/",
+	pack_output_dir = pack_root + "output/";
 
 gulp.task("bundle-svg", function(){
 	var pack_name = "floor";
@@ -36,5 +42,24 @@ gulp.task("bundle-svg", function(){
 		.pipe(gulp.dest(pack_output_dir));
 });
 
-// build script
-gulp.task("build", ["bundle-svg"]);
+/*
+ * Compile SCSS
+ */
+
+// Compile .scss files within src/stylesheets/scss
+gulp.task('scss', function () {
+	return gulp.src('src/stylesheets/scss/**/*.scss')
+	           .pipe(sass().on('error', sass.logError))
+	           .pipe(gulp.dest('src/stylesheets/compiled/'));
+});
+
+// .scss Watcher
+gulp.task('scss:watch', function () {
+	gulp.watch('src/stylesheets/scss/**/*.scss', ['scss']);
+});
+
+
+/*
+ * Build
+ */
+gulp.task("build", ["scss"]);
