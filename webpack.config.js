b609e39bc1314fb7d99a0780a473784b2f7f933a
Kevin Law <hao_21291@yahoo.com>
2016-07-29 23:16:05 +0800

    Configure Babel

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,15 @@ module.exports = {
 	resolve: {
 		extensions: ["", ".js"]
 	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: "babel?cacheDirectory",
+				include: path.join(__dirname, "src")
+			}
+		]
+	},
 	plugins: [
 		new webpack.DefinePlugin({
 			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
