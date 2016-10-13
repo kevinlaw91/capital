9081ec8426a7351ad809724e90f3864fc51d87ff
Kevin Law <hao_21291@yahoo.com>
2016-10-14 01:39:34 +0800

    Added padStart polyfill

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,9 @@ import { AppContainer as ReactAppWrapper } from "react-hot-loader";
 import store from "./redux";
 import { Provider } from "react-redux";
 
+// Polyfills
+import "./js/polyfills";
+
 // Main App
 import App from "./ui/App";
 
