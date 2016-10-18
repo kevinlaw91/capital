47795e6ab74e300875b0252b0dc5fce9c482173b
Kevin Law <hao_21291@yahoo.com>
2016-10-18 18:55:20 +0800

    Create demo player

--- src/game/session.js
+++ src/game/session.js
@@ -1,5 +1,6 @@
 import dispatch from "./utils/dispatch";
 import { actions as mapActions } from "../redux/game/session/map";
+import { actions as playerActions } from "../redux/game/session/players";
 
 /**
  * Current active game session
@@ -12,6 +13,9 @@ export let active = null;
 export function create() {
 	// Generate map
 	dispatch(mapActions.generate());
+
+	// Create a demo player
+	dispatch(playerActions.add());
 }
 
 /** Destroy active game session */
