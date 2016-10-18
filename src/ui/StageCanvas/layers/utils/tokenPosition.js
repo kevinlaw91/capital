e9c35baff7090156f9ca67e15fbabc83248cf892
Kevin Law <hao_21291@yahoo.com>
2016-10-18 19:41:55 +0800

    Refactor mapping function of tokenPosition

--- src/ui/StageCanvas/layers/utils/tokenPosition.js
+++ src/ui/StageCanvas/layers/utils/tokenPosition.js
@@ -1,16 +1,29 @@
-export default function (side, index) {
-	return {
-		"CORNER-BOTTOM": i => ({ x: 13, y: 13 }),
-		"CORNER-LEFT": i => ({ x: 3, y: 13 }),
-		"CORNER-RIGHT": i => ({ x: 13, y: 3 }),
-		"CORNER-TOP": i => ({ x: 3, y: 3 }),
-		// x = 12, 11, 10 ... 4
-		"S": i => ({ x: 12 - i, y: 13 }),
-		// y = 12, 11, 10 ... 4
-		"W": i => ({ x: 3, y: 12 - i }),
-		// x = 4, 5, 6 ... 12
-		"N": i => ({ x: 4 + i, y: 3 }),
-		// y = 4, 5, 6 ... 12
-		"E": i => ({ x: 13, y: 4 + i }),
-	}[side](index);
+export default function (locationId) {
+	// Test to see if location is a property lot
+	// and attempt to extract fields from it
+	const lot = /LOT-(N|S|E|W)(\d{2})/.exec(locationId);
+
+	if (lot) {
+		// Location id represents a property lot
+		// Extract side and index from it
+		const [/* skip */, side, idx] = lot;
+
+		// Strip zero padding
+		let i = Number(idx);
+
+		switch (side) {
+			case "S": return { x: 12 - i, y: 13 };
+			case "W": return { x: 3, y: 12 - i };
+			case "N": return { x: 4 + i, y: 3 };
+			case "E": return { x: 13, y: 4 + i };
+		}
+	} else {
+		// Not property lot
+		switch (locationId) {
+			case "CORNER-BOTTOM": return { x: 13, y: 13 };
+			case "CORNER-LEFT": return { x: 3, y: 13 };
+			case "CORNER-RIGHT": return { x: 13, y: 3 };
+			case "CORNER-TOP": return { x: 3, y: 3 };
+		}
+	}
 }
