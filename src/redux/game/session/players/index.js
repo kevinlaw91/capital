a6c5976694054a49cd1f5bfa3e107b9078a11686
Kevin Law <hao_21291@yahoo.com>
2016-10-18 19:05:03 +0800

    Add player setPosition

--- src/redux/game/session/players/index.js
+++ src/redux/game/session/players/index.js
@@ -5,12 +5,19 @@ import shortid from "shortid";
 export const types = {
 	"ADD": "game/session/players/ADD",
 	"CLEAR": "game/session/players/CLEAR",
+	"SET_POSITION": "game/session/players/SET_POSITION",
 };
 
 // Actions
 export const actions = {
 	add: () => ({ type: types.ADD }),
 	clear: () => ({ type: types.CLEAR }),
+
+	setPosition: (playerId, pos) => ({
+		type: types.SET_POSITION,
+		id: playerId,
+		positon: pos,
+	})
 };
 
 // Initial state
@@ -29,6 +36,12 @@ export function reducer(state = initialState, action = {}) {
 			} while (id in state);
 
 			return state.merge({ [id]: {} }, { deep: true });
+
+		case types.SET_POSITION:
+			return state.merge(
+				{ [action.id]: {
+					position: action.position
+				} }, { deep: true });
 
 		case types.CLEAR:
 			return initialState;
