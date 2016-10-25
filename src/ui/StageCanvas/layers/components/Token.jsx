0032d44623506e58f2ad589f2c0babf30c5e301a
Kevin Law <hao_21291@yahoo.com>
2016-10-25 15:23:55 +0800

    Token move animation

--- src/ui/StageCanvas/layers/components/Token.jsx
+++ src/ui/StageCanvas/layers/components/Token.jsx
@@ -1,20 +1,27 @@
+import { VelocityComponent } from "velocity-react";
+
 import { tokens as sprite } from "../../../utils/sprites";
 
 // Offset to be applied to the sprite
 const offsetY = -30;
 
 export default function Token(props) {
+	const animatedProps = {
+		x: props.x,
+		y: props.y + offsetY,
+	};
+
 	return (
-		<use
-			x={props.x}
-			y={props.y + offsetY}
-			width={64}
-			height={64}
-			xlinkHref={ sprite(props.color) }
-			style={{
-				"shapeRendering": "crispEdges",
-			}}
-		/>
+		<VelocityComponent animation={animatedProps} duration={300}>
+			<use
+				width={64}
+				height={64}
+				xlinkHref={ sprite(props.color) }
+				style={{
+					"shapeRendering": "crispEdges",
+				}}
+			/>
+		</VelocityComponent>
 	);
 }
 
