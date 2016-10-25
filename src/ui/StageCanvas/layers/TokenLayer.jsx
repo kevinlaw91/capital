06b455941bb7eb18f82d0edfd447077bfbfad2d0
Kevin Law <hao_21291@yahoo.com>
2016-10-26 00:40:36 +0800

    Active token always on top

--- src/ui/StageCanvas/layers/TokenLayer.jsx
+++ src/ui/StageCanvas/layers/TokenLayer.jsx
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import Token from "./components/Token";
 
 import { selectAllTokens } from "../../../redux/game/stage/tokens";
+import {
+	selectActivePlayerId,
+	selectTurnOrder,
+} from "../../../redux/game/session/turn";
 
 function renderToken(id, entry) {
 	return (
@@ -17,18 +21,33 @@ function renderToken(id, entry) {
 }
 
 function TokenLayer(props) {
+	// Render active token separately
+	// to make it always on top
+	let inactive, active;
+	if (props.order && props.active) {
+		inactive = props.order
+		                .filter(token => token !== props.active)
+		                .map(k => renderToken(k, props.tokens[k]));
+		active = renderToken(props.active, props.tokens[props.active]);
+	}
+
 	return (
 		<g>
-			{ Object.keys(props.tokens).map(k => renderToken(k, props.tokens[k])) }
+			{inactive}
+			{active}
 		</g>
 	);
 }
 
 TokenLayer.propTypes = {
 	tokens: React.PropTypes.object,
+	order: React.PropTypes.arrayOf(React.PropTypes.string),
+	active: React.PropTypes.string,
 };
 
 const mapStateToProps = state => ({
+	active: selectActivePlayerId(state),
+	order: selectTurnOrder(state),
 	tokens: selectAllTokens(state),
 });
 
