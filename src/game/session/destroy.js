529b09020e73a7274234adce379fb44df455d84f
Kevin Law <hao_21291@yahoo.com>
2016-10-31 04:13:18 +0800

    Removed unused code from session/destroy

--- src/game/session/destroy.js
+++ src/game/session/destroy.js
@@ -1,16 +1,2 @@
-import dispatch from "../utils/dispatch";
-
-import { actions as playerCollection } from "../../redux/game/session/players";
-import { actions as tokenCollection } from "../../redux/game/stage/token/items";
-import { actions as map } from "../../redux/game/session/map";
-
 export default function () {
-	// Reset map
-	dispatch(map.reset());
-
-	// Clear player list
-	dispatch(playerCollection.clear());
-
-	// Clear player tokens
-	dispatch(tokenCollection.clear());
 }
