f59af3b4694d825684c9484d4607e0fe2a4c788f
Kevin Law <hao_21291@yahoo.com>
2016-11-22 07:45:13 +0800

    Supporting string input for is_unowned check

--- src/game/rules/map/lot/condition/is_unowned.js
+++ src/game/rules/map/lot/condition/is_unowned.js
@@ -1,1 +1,14 @@
-export default location => !location.owner;
+import getState from "redux/getState";
+import { selectEntityById } from "redux/game/session/map";
+
+export default location => {
+	let resolvedLocation;
+	if (typeof location === "string") {
+		// Resolve id to entity
+		resolvedLocation = selectEntityById(getState(), location);
+	} else {
+		resolvedLocation = location;
+	}
+
+	return !resolvedLocation.owner;
+};
