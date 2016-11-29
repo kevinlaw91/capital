7ec1bcfaa9cd65f853f5aeea4d23592c8f2dbe07
Kevin Law <hao_21291@yahoo.com>
2016-11-30 03:02:15 +0800

    Show rent income floater above owner token

--- src/game/rules/map/lot/pay_rent.js
+++ src/game/rules/map/lot/pay_rent.js
@@ -2,6 +2,7 @@ import dispatch from "redux/dispatch";
 import getState from "redux/getState";
 import { actions as playerActions } from "redux/player";
 import { selectPlayerById } from "redux/game/session/players";
+import { selectEntityById } from "redux/game/session/map";
 import { getStageInstance } from "game/session/stage";
 import { templates } from "redux/game/stage/floaters";
 import getRent from "game/rules/map/lot/getRent";
@@ -10,23 +11,31 @@ import { getScreenOffset } from "game/map/tile/tokenPosition";
 export default (location, player) => {
 	// Amount
 	const amount = getRent(location);
-	const owner = selectPlayerById(getState(), location.owner);
 
 	// Deduct gold
 	dispatch(playerActions.deductGold(player.id, amount));
 
-	// Render GoldChangeText floater
-	let x, y;
-	({ x, y } = getScreenOffset(location.id));
+	// Render GoldChangeText floaters
+	const state = getState();
+	const owner = selectPlayerById(state, location.owner);
+
+	const offsetActivePlayer = getScreenOffset(location.id);
+	const offsetOwner = getScreenOffset(owner.position);
 
 	const floaters = getStageInstance().floaters;
+
+	// Behind
 	floaters.addItem(templates.GOLD_CHANGE_TEXT, {
-		x, y,
+		x: offsetOwner.x,
+		y: offsetOwner.y,
 		text: `+${amount}`,
 		color: owner.color,
 	});
+
+	// Front
 	floaters.addItem(templates.GOLD_CHANGE_TEXT, {
-		x, y,
+		x: offsetActivePlayer.x,
+		y: offsetActivePlayer.y,
 		text: `-${amount}`,
 		color: player.color,
 	});
