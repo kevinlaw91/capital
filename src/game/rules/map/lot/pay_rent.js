a3599ed2c2fddb61a32c5e1167e3f0788e299d6e
Kevin Law <hao_21291@yahoo.com>
2016-11-29 23:14:37 +0800

    Pay rent now show floater for income

--- src/game/rules/map/lot/pay_rent.js
+++ src/game/rules/map/lot/pay_rent.js
@@ -1,5 +1,7 @@
 import dispatch from "redux/dispatch";
+import getState from "redux/getState";
 import { actions as playerActions } from "redux/player";
+import { selectPlayerById } from "redux/game/session/players";
 import { getStageInstance } from "game/session/stage";
 import { templates } from "redux/game/stage/floaters";
 import getRent from "game/rules/map/lot/getRent";
@@ -8,6 +10,7 @@ import { getScreenOffset } from "game/map/tile/tokenPosition";
 export default (location, player) => {
 	// Amount
 	const amount = getRent(location);
+	const owner = selectPlayerById(getState(), location.owner);
 
 	// Deduct gold
 	dispatch(playerActions.deductGold(player.id, amount));
@@ -16,7 +19,13 @@ export default (location, player) => {
 	let x, y;
 	({ x, y } = getScreenOffset(location.id));
 
-	getStageInstance().floaters.addItem(templates.GOLD_CHANGE_TEXT, {
+	const floaters = getStageInstance().floaters;
+	floaters.addItem(templates.GOLD_CHANGE_TEXT, {
+		x, y,
+		text: `+${amount}`,
+		color: owner.color,
+	});
+	floaters.addItem(templates.GOLD_CHANGE_TEXT, {
 		x, y,
 		text: `-${amount}`,
 		color: player.color,
